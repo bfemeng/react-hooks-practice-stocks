909554b50ae734ad41b2770594dd2125cd3acb6c
ShanteTorres <kennedy2110@gmail.com>
2022-03-20 22:29:09 -0400

    test

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-function SearchBar() {
+function SearchBar({ sortStocks, sortBy, filterBy, onChangeFilter }) {
+
+
   return (
     <div>
       <strong>Sort by:</strong>
@@ -9,8 +11,8 @@ function SearchBar() {
           type="radio"
           value="Alphabetically"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={ sortBy === 'Alphabetically' }
+          onChange={sortStocks}
         />
         Alphabetically
       </label>
@@ -19,15 +21,15 @@ function SearchBar() {
           type="radio"
           value="Price"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={ sortBy === 'Alphabetically' }
+          onChange={sortStocks}
         />
         Price
       </label>
       <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={onChangeFilter} value={filterBy} >
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
